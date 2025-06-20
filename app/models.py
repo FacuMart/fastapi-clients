b1e9c4b825cdf1b8d3bf7b05dc8d2cc2aaa9272e
FacuMart <facundomartinez.3000@gmail.com>
2025-06-20 00:24:06 -0300

    master: rename tablename on models.py

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String
 from .database import Base
 
 class Cliente(Base):
-    __tableame__ = "clientes"
+    __tablename__ = "clientes"
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String)
     email = Column(String)
